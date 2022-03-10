469b11d640c0311849a90cd7edf40fb9c4098b65
Kiko Sala <kikinsala@gmail.com>
2022-03-11 00:23:07 +0100

    Cambios varios y metodos de primitiva

--- src/main/java/Primitiva.java
+++ src/main/java/Primitiva.java
@@ -1,6 +1,6 @@
 public class Primitiva {
     public enum Categoria{
-        ESPECIAL, PRIMERA, SEGUNDA, TERCERA, CUARTA, QUINTA
+        PRIMERA, SEGUNDA, TERCERA, CUARTA, QUINTA, REINTEGRO, ESPECIAL
     }
 
     private Boleto boletoGanador;
@@ -9,7 +9,7 @@ public class Primitiva {
         boletoGanador = new Boleto();
     }
 
-    public Categoria comproabarCategoria(Boleto boleto){
+    public Categoria jugar(Boleto boleto){
         int aciertos = 0;
         for(int i = 0; i < boletoGanador.getCombinacion().length; i++){
             for(int j = 0; j < boleto.getCombinacion().length; j++){
@@ -33,32 +33,51 @@ public class Primitiva {
                 }else{
                     return Categoria.PRIMERA;
                 }
-            default: return null;
+            default: if(boleto.getReintegro() == boletoGanador.getReintegro()){
+                return Categoria.REINTEGRO;
+            }
         }
+        return null;
     }
 
     public Boleto getBoletoGanador() {
         return boletoGanador;
     }
 
-    public String jugarUnico() {
-        return null;
+    public Categoria jugarHastaPremio() {
+        Categoria categoria = null;
+        while(categoria == null){
+            categoria = jugar(new Boleto());
+        }
+        return categoria;
+    }
+    public Categoria jugarHastaPremioSinReintegro() {
+        Categoria categoria = null;
+        while(categoria == null || categoria.equals(Categoria.REINTEGRO)){
+            categoria = jugar(new Boleto());
+        }
+        return categoria;
     }
 
-    public String jugarHastaPremio() {
-        return null;
+    public int[] cicloDe10000Sorteos() {
+        final int limite = 10000;
+        int[] resultado = new int[Categoria.values().length];
+        for(int i = 0; i < limite; i++){
+            switch(jugar(new Boleto())){
+                case PRIMERA -> resultado[0] += 1;
+                case SEGUNDA -> resultado[1] += 1;
+                case TERCERA -> resultado[2] += 1;
+                case CUARTA -> resultado[3] += 1;
+                case QUINTA -> resultado[4] += 1;
+                case REINTEGRO -> resultado[5] += 1;
+                case ESPECIAL -> resultado[6] += 1;
+            }
+        }
+        return resultado;
     }
 
-    public String jugarHastaPremioSinReintegro() {
-        return null;
-    }
-
-    public String cicloDe10000Sorteos() {
-        return null;
-    }
-
-    public String jugarHastaCategoriaEspecial() {
-        return null;
+    public long jugarHastaCategoriaEspecial() {
+        
     }
 
 }
